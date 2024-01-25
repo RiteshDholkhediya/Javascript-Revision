c0b10f5eaac5c0552a774501d9c5555ef326872a
RiteshDholkhediya <riteshkheda444@gmail.com>
2024-01-26 00:06:27 +0530

    Conversion of datatpes

--- 01_basics/02_daaTypes.js
+++ 01_basics/02_daaTypes.js
@@ -1,9 +1,10 @@
-"use strict"; // treat all JS code as newer version
+//"use strict"; // treat all JS code as newer version
 
-// alert( 3 + 3) // we are using nodejs, not browser
+ alert( 3 + 3); // we are using nodejs, not browser
 
-console.log(3 + 3); // code readability should be high
+//console.log(3 + 3); // code readability should be high
 
+/*
 console.log("Hitesh");
 
 let name = "hitesh";
@@ -11,6 +12,7 @@ let age = 18;
 let isLoggedIn = false;
 let state;
 
+*/
 // number => 2 to power 53
 // bigint
 // string => ""
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33abc";
+
+//console.log(typeof score);
+//console.log(typeof score);
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); // number
+//console.log(valueInNumber); // NaN
+
+/*
+Note : 
+
+// "33" = 33;
+// "33abc" = NaN;
+// true = 1; false = 0;
+// {} => NaN
+
+*/
+
+let isLoggedIn = 0;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "hitesh" => true
+// {} => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber); // 33
+console.log(typeof stringNumber); // string
