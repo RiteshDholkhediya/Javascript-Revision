0524a1e7f8b2f71e02c6ecc17ca117d6313ad935
RiteshDholkhediya <riteshkheda444@gmail.com>
2024-01-30 23:36:19 +0530

    Learnt about objects in js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,4 +2,34 @@
 // Object.create
 
 //Object literals
-const JsUser = {};
+
+const mySym = Symbol("key1");
+
+const JsUser = {
+  name: "Hitesh",
+  "full name": "Hitesh Choudhary",
+  [mySym]: "mySym1",
+  age: 18,
+  location: "delhi",
+  email: "ritesh@google.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
+
+console.log(JsUser.email);
+console.log(JsUser["full name"]);
+console.log(typeof JsUser[mySym]);
+
+JsUser.email = "hitesh@chatgpt.com";
+// Object.freeze(JsUser); // after this line we cant change the object
+JsUser.email = "hitesh@microsoft.com";
+console.log(JsUser);
+
+JsUser.greeting = function () {
+  console.log("Hello JS user");
+};
+JsUser.greeting = function () {
+  console.log(`Hello JS user ${this.name}`);
+};
+
+console.log(JsUser.greeting());
