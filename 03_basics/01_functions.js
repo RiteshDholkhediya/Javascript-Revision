28cd188627b46328caca9aaf365818bda5993d7c
RiteshDholkhediya <riteshkheda444@gmail.com>
2024-02-01 13:30:53 +0530

    learnt fucntions with objects

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,23 @@
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(200, 120, 300, 4000));
+
+const user = {
+  username: "Hitesh",
+  price: 199,
+};
+
+function handleObject(anyObj) {
+  console.log(`Username is ${anyObj.username} and Price is ${anyObj.price}`);
+}
+
+// handleObject(user);
+
+const myNewArray = [200, 400, 100, 600];
+function returnSecondVal(getArray) {
+  return getArray[1];
+}
+
+// console.log(returnSecondVal(myNewArray));
